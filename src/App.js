a3d2cbdcf07a9cf064d3f93f6367398a3d40a308
Miranda Lopez <mirandaineslopez@gmail.com>
2021-01-04 21:49:36 -0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+
+// Data hardcoded
+let days;
+let hours;
+let minutes;
+let seconds;
+
+let date = new Date("Sep 23 2022 00:00:00");
+let now = new Date("Sep 23 2021 00:00:00");
+
+const getIntervalTime = (startDate, endDate) => {
+	let secondsInterval = Math.floor((endDate - startDate) / 1000);
+	return secondsInterval;
+};
+
+const getDaysHoursMinutesSeconds = (interval) => {
+	days = Math.floor(interval / (24 * 60 * 60));
+	hours = Math.floor((interval % (24 * 60 * 60)) / (60 * 60));
+	minutes = Math.floor((interval % (60 * 60)) / 60);
+	seconds = Math.floor((interval % 60) / 1);
+	return (interval = {
+		d: days,
+		h: hours,
+		m: minutes,
+		s: seconds,
+	});
+};
+
+const countdown = (n) => {
+	return n - 1;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let intervalDate;
+	let intervalSeconds;
+
+	intervalSeconds = getIntervalTime(now, date);
+	let { d, h, m, s } = getDaysHoursMinutesSeconds(intervalSeconds);
+	setInterval(() => {
+		intervalSeconds = countdown(intervalSeconds);
+		intervalDate = getDaysHoursMinutesSeconds(intervalSeconds);
+		d = document.getElementById("d1").innerText = intervalDate.d;
+		d = document.getElementById("d2").innerText = intervalDate.d;
+		h = document.getElementById("h1").innerText = intervalDate.h;
+		h = document.getElementById("h2").innerText = intervalDate.h;
+		m = document.getElementById("m1").innerText = intervalDate.m;
+		m = document.getElementById("m2").innerText = intervalDate.m;
+		s = document.getElementById("s1").innerText = intervalDate.s;
+		s = document.getElementById("s2").innerText = intervalDate.s;
+	}, 1000);
+
+	return (
+		<section className='App'>
+			<div className='main-container'>
+				<h3>We're launching soon</h3>
+				<div className='date-container'>
+					<div className='single-card'>
+						<h1 id='d1' className='card dark'>
+							{d}
+						</h1>
+						<h1 id='d2' className='card light'>
+							{d}
+						</h1>
+
+						<h3 className='date-label'>Days</h3>
+					</div>
+					<div className='single-card'>
+						<h1 id='h1' className='card dark'>
+							{h}
+						</h1>
+						<h1 id='h2' className='card light'>
+							{h}
+						</h1>
+
+						<h3 className='date-label'>Hours</h3>
+					</div>
+					<div className='single-card'>
+						<h1 id='m1' className='card dark'>
+							{m}
+						</h1>
+						<h1 id='m2' className='card light'>
+							{m}
+						</h1>
+
+						<h3 className='date-label'>Minutes</h3>
+					</div>
+					<div className='single-card'>
+						<h1 id='s1' className='card dark'>
+							{s}
+						</h1>
+						<h1 id='s2' className='card light'>
+							{s}
+						</h1>
+
+						<h3 className='date-label'>Seconds</h3>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default App;
